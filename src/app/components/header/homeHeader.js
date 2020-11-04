0667f7cc1247e8b4c6a85ce6b7c850f429f57e00
Justin <justin.vanvoorst@motili.com>
2020-11-04 11:46:22 -0700

    add table and item cards

--- src/app/components/header/homeHeader.js
+++ src/app/components/header/homeHeader.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import './homeHeader.css';
 import Button from '../button/button';

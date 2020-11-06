d26365328d864cac9d3c8ef1fb18d4cb6f578cf7
Justin <justin.vanvoorst@motili.com>
2020-11-06 14:58:59 -0700

    add production ports

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,16 @@
-export const APIBase = 'http://localhost:3000/api/';
-export const socketIOEndpoint = 'http://localhost:3000';
+
+let APIBase;
+let socketIOEndpoint;
+
+if (process.env.deployment === 'production') {
+    APIBase = 'https://josh-server-vlu5i.ondigitalocean.app/api/';
+    socketIOEndpoint = 'https://josh-server-vlu5i.ondigitalocean.app';
+} else {
+    APIBase = 'http://localhost:3000/api/';
+    socketIOEndpoint = 'http://localhost:3000';
+}
+
+export {
+    APIBase,
+    socketIOEndpoint
+};

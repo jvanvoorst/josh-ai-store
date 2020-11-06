6efcf86790ba72afbc80d6b74d59def4a3b5c55f
Justin <justin.vanvoorst@motili.com>
2020-11-06 14:14:58 -0700

    update ports

--- src/app/config.js
+++ src/app/config.js
@@ -1,2 +1,2 @@
-export const APIBase = 'http://localhost:3001/api/';
-export const socketIOEndpoint = 'http://localhost:3001';
+export const APIBase = 'http://localhost:3000/api/';
+export const socketIOEndpoint = 'http://localhost:3000';

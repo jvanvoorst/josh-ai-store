ed6c0b8f7adf4c47f8d8748a25c20f875705b270
Justin <justin.vanvoorst@motili.com>
2020-11-11 17:08:40 -0700

    update prod api

--- src/app/config.js
+++ src/app/config.js
@@ -1,6 +1,6 @@
 
-// export const APIBase = 'https://josh-server-vlu5i.ondigitalocean.app/api/';
-// export const socketIOEndpoint = 'https://josh-server-vlu5i.ondigitalocean.app';
+export const APIBase = 'https://josh-server-vlu5i.ondigitalocean.app/api/';
+export const socketIOEndpoint = 'https://josh-server-vlu5i.ondigitalocean.app';
 
-export const APIBase = 'http://localhost:3000/api/';
-export const socketIOEndpoint = 'http://localhost:3000';
+// export const APIBase = 'http://localhost:3000/api/';
+// export const socketIOEndpoint = 'http://localhost:3000';
